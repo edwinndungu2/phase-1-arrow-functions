681ea183a1da11e000e8e60cd1c05b3bfc65b09a
edwinndungu <edwin.ndungu2@student.moringaschool.com>
2025-03-10 19:37:39 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,46 @@
+function divide() {
+    let a = 2000
+    let b = 100
+    return a / b
+
+    console.log(divide);
+
+
+}   
+
+
+
+
+console.log(divide(2000, 100)); 
+
+
+
+
+function square() {
+    let x = 2   
+
+  return x * x
+  console.log(square);
+}   
+
+console.log(square(2));
+
+   
+
+
+
+function add() {
+    let a = 3
+    let b = 4       
+
+  return a + b  
+  console.log(add);
+
+}
+console.log(add(3, 4))
+
+
+
+;
+
+
